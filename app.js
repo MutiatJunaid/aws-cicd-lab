a978e5fb958a32ff8e570327fcd8607ae8f2ca31
Mutiat Junaid <mutiatjunaid@gmail.com>
2026-06-06 00:17:08 +0100

    made a change on app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('🚀 CI/CD Deployment Successful!');
+  res.send('🚀 jan cohort CI/CD Deployment Successful!');
 });
 
 app.listen(PORT, () => {
